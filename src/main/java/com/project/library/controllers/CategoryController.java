4624055d70fb8e83adc9a20b96578d66d1410a95
Halan Caio <hallankayo20@gmail.com>
2023-10-29 01:50:44 -0300

    Post and delete methods to CategoryController and CategoryService

--- src/main/java/com/project/library/controllers/CategoryController.java
+++ src/main/java/com/project/library/controllers/CategoryController.java
@@ -1,11 +1,18 @@
 package com.project.library.controllers;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.project.library.entities.Category;
 import com.project.library.services.CategoryService;
@@ -20,5 +27,17 @@ public class CategoryController {
 	@GetMapping
 	public List<Category> findAll(){
 		return categoryService.findAll();
+	}
+	
+	@PostMapping
+	public ResponseEntity<Category> addCategory(@RequestBody Category category){
+		category = categoryService.addCategory(category);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/id").buildAndExpand(category.getId()).toUri();
+		return ResponseEntity.created(uri).body(category);
+	}
+	
+	@DeleteMapping(value = "/{id}")
+	public void deleteCategory(@PathVariable Long id) {
+		categoryService.deleteCategory(id);
 	}
 }
--- src/main/java/com/project/library/services/CategoryService.java
+++ src/main/java/com/project/library/services/CategoryService.java
@@ -3,10 +3,14 @@ package com.project.library.services;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.project.library.entities.Category;
 import com.project.library.repositories.CategoryRepository;
+import com.project.library.services.exceptions.DatabaseException;
+import com.project.library.services.exceptions.ResourceNotFoundException;
 
 import jakarta.transaction.Transactional;
 
@@ -14,11 +18,28 @@ import jakarta.transaction.Transactional;
 public class CategoryService {
 
 	@Autowired
-	private CategoryRepository category;
+	private CategoryRepository categoryRepository;
 	
 	@Transactional
 	public List<Category> findAll(){
-		List<Category> response = category.findAll();
+		List<Category> response = categoryRepository.findAll();
 		return response;
+	}
+	
+	public Category addCategory(Category category) {
+		return categoryRepository.save(category);
+	}
+
+	public void deleteCategory(Long id) {
+		try {
+			categoryRepository.deleteById(id);
+		}
+		catch(EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);
+		}
+		catch(DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
+		}
+		
 	}
 }
