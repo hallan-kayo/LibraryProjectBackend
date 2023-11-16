6b4acaa5efdb75c9a0bd628af2ad2e358ae8c479
Halan Caio <hallankayo20@gmail.com>
2023-11-15 22:07:13 -0300

    exception criada

--- src/main/java/com/project/library/controllers/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/project/library/controllers/exceptions/ResourceExceptionHandler.java
@@ -17,26 +17,29 @@ import jakarta.servlet.http.HttpServletRequest;
 public class ResourceExceptionHandler {
 
 	@ExceptionHandler(ResourceNotFoundException.class)
-	public ResponseEntity<StandardError> resourceNotFound(ResourceNotFoundException e, HttpServletRequest request){
+	public ResponseEntity<StandardError> resourceNotFound(ResourceNotFoundException e, HttpServletRequest request) {
 		String error = "Resource not found.";
 		HttpStatus status = HttpStatus.NOT_FOUND;
-		StandardError err = new StandardError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
+		StandardError err = new StandardError(Instant.now(), status.value(), error, e.getMessage(),
+				request.getRequestURI());
 		return ResponseEntity.status(status).body(err);
 	}
-	
+
 	@ExceptionHandler(DatabaseException.class)
-	public ResponseEntity<StandardError> database(DatabaseException e, HttpServletRequest request){
+	public ResponseEntity<StandardError> database(DatabaseException e, HttpServletRequest request) {
 		String error = "Database error.";
 		HttpStatus status = HttpStatus.BAD_REQUEST;
-		StandardError err = new StandardError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
+		StandardError err = new StandardError(Instant.now(), status.value(), error, e.getMessage(),
+				request.getRequestURI());
 		return ResponseEntity.status(status).body(err);
 	}
-	
-	 @ExceptionHandler(DeleteException.class)
-	    public ResponseEntity<StandardError> deleteException(DeleteException e, HttpServletRequest request) {
-		 String error = "Integrity Violation";
-			HttpStatus status = HttpStatus.CONFLICT;
-			StandardError err = new StandardError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
-			return ResponseEntity.status(status).body(err);
-	    }
+
+	@ExceptionHandler(DeleteException.class)
+	public ResponseEntity<StandardError> deleteException(DeleteException e, HttpServletRequest request) {
+		String error = "Integrity Violation";
+		HttpStatus status = HttpStatus.CONFLICT;
+		StandardError err = new StandardError(Instant.now(), status.value(), error, e.getMessage(),
+				request.getRequestURI());
+		return ResponseEntity.status(status).body(err);
+	}
 }
